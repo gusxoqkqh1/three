684f232f31070796524534015821f1dc7f4e7320
ubuntu <a01026676497@gmail.com>
2021-04-26 15:42:18 +0900

    del : my_settings.py2

---
+++ my_setting.py
@@ -0,0 +1,11 @@
+DATABASES = {
+'default' : {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'secend', 
+'USER': 'root',
+'PASSWORD': 'ubuntu', 
+'HOST': 'localhost',
+'PORT': '3306',
+}
+}
+SECRET = '=6xvu3t)6+8k8vi@b-(g4=2ah$@vi*^f@h#fraf)mc-_3@xw8t'
